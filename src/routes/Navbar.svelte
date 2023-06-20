be2adb4b324b560ee021bb606ee44a0fdd25b2fe
Anonymous <anony@mou.us>
2023-06-20 17:38:03 +0800

    Better Navbar

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,16 +1,33 @@
 <script>
 	import { user, authenticated, authenticating, token } from "$lib/stores/store";
 </script>
-<nav>
-	<a href="/">Home</a>
-	<a href="/about">About</a>
-    {#if $authenticated}
-        <a href="/tasks">Tasks</a>
-        {#if $token?.accessToken.abilities.includes('ADMINISTRATOR')}
-            <a href="/admin">Admin</a>
+<nav class="container-fluid">
+    <ul>
+        <a href="/" class="contrast">
+            <strong>Tubes Frontend</strong>
+        </a>
+    </ul>
+    <ul>
+        <li><a href="/">Home</a></li>
+        {#if $authenticated}
+            <li><a href="/new_project">New Project</a></li>
+            <li><a href="/ongoing_project">Ongoing Project</a></li>
+            <li><a href="/ongoing_project">Tool History</a></li>
+    
+            {#if $token?.accessToken.abilities.includes('ADMINISTRATOR')}
+            <li>
+                <details role="list" dir="rtl">
+                <summary aria-haspopup="listbox" role="link">Admin</summary>
+                <ul role="listbox" data-theme="dark">
+                    <li><a href="#a">Users</a></li>
+                    <li><a href="#a">Tools</a></li>
+                </ul>
+                </details>
+            </li>
+            {/if}
+            <li><a href="/logout">Logout</a></li>
+        {:else}
+            <li><a href="/login">Login</a></li>
         {/if}
-        <a href="/logout">Logout</a>
-    {:else}
-	    <a href="/login">Login</a>
-    {/if}
+    </ul>
 </nav>
