9b1aed7e2ef96a2a56dcd50e6f3337c3d6c124ae
Anonymous <anony@mou.us>
2023-06-21 18:49:38 +0800

    Toast customization

--- src/lib/components/Toast.svelte
+++ src/lib/components/Toast.svelte
@@ -5,12 +5,29 @@
 <div class="toast">
 	<SvelteToast
 		options={{
-			classes: ['something']
+			classes: ['toast']
 		}}
 	/>
 </div>
 
 <style>
+	
+	:global(.toast) {
+		--toastBackground: #525252;
+		--toastBarBackground: #d4d4d4;
+	}
+	:global(.toast.success) {
+		--toastBackground: #16a34a;
+		--toastBarBackground: #a3e6be;
+	}
+	:global(.toast.danger) {
+		--toastBackground: #dc2626;
+		--toastBarBackground: #fca5a5;
+	}
+	:global(.toast.warning) {
+		--toastBackground: #ca8a04;
+		--toastBarBackground: #fde68a;
+	}
 	:global(ul._toastContainer>li) {
 		list-style: none;
 	}
