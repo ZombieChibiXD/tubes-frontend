83ba368691b0a9480dcf01c7aea89e7cc71eadff
Anonymous <anony@mou.us>
2023-06-21 22:41:43 +0800

    Main Page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,7 +15,7 @@
 		<header>
 			<h2>What is this Program About?</h2>
 			<p>
-				This is a simple program that allows you to create a list of tasks to do.
+				This is a simple program that allows you to manage your tools and their status.
 			</p>
 		</header>
 		<main>
@@ -23,13 +23,6 @@
 			<p>
 				You first have to login to be able to use the program. If you don't have an account, you must request one to the administrator.
 			</p>
-			<p>
-				Once you are logged in, you can create a new task by clicking on the "New Task" button. You can also edit or delete a task by clicking on the corresponding buttons.
-			</p>
-			<p>
-				You can also filter the tasks by their status (pending, in progress, done) or by their priority (low, medium, high).
-			</p>
-
 		</main>
 	</article>
 </section>
