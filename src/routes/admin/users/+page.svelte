b1f60dbabc7542be60a0b484703ca82287994859
Anonymous <anony@mou.us>
2023-06-26 20:19:17 +0800

    Refactored User page

--- src/routes/admin/users/+page.svelte
+++ src/routes/admin/users/+page.svelte
@@ -4,8 +4,9 @@
 	import { fetch_api } from '$lib/components/fetch';
 	import { toasts } from '$lib/components/toasts';
 	import { authenticated } from '$lib/stores/store';
-	import { onDestroy, onMount } from 'svelte';
+	import { onDestroy } from 'svelte';
 
+	let loading = true;
 	/**
 	 * @type {{ [key: string]: string[] }}
 	 */
@@ -16,10 +17,10 @@
 	 */
 	let users = [];
 
-    /**
-     * @type {import('$lib/types/global').Role[]}
-     */
-    let server_roles = [];
+	/**
+	 * @type {import('$lib/types/global').Role[]}
+	 */
+	let server_roles = [];
 
 	const interval = setInterval(() => {
 		if ($authenticated) {
@@ -37,11 +38,11 @@
 
 		if (result.status == 200) {
 			users = await result.json();
-            result = await fetch_api('api/roles');
-            if (result.status == 200) {
-                server_roles = await result.json();
-                return;
-            }
+			result = await fetch_api('api/roles');
+			if (result.status == 200) {
+				server_roles = await result.json();
+				return;
+			}
 		}
 		const { message, errors: json_err } = await result.json();
 
@@ -55,7 +56,7 @@
 	}
 	$: {
 		if ($authenticated) {
-			load();
+			load().finally(() => (loading = false));
 		}
 	}
 
@@ -78,90 +79,101 @@
 	 *   role_ids: number[]
 	 * }}
 	 */
-    let user = {
-        id: null,
-        name: '',
-        username: '',
-        email: '',
-        password: undefined,
-        role_ids: []
-    };
+	let user = {
+		id: null,
+		name: '',
+		username: '',
+		email: '',
+		password: undefined,
+		role_ids: []
+	};
 
 	async function handleSubmit() {
-        errors = {};
+		errors = {};
 		if (user.id) {
 			if (!user.password || user.password.length == 0) {
 				user.password = undefined;
 			}
 		}
-		const [url, options] = user.id ? ['api/users/' + user.id, {
-			method: 'PUT',
-			body: JSON.stringify(user)
-		}] : ['api/users', {
-            method: 'POST',
-            body: JSON.stringify(user)
-        }];
-        const result = await fetch_api(url, options);
-        if (result.status == 201 || result.status == 200) {
-            const user = await result.json();
-            users[user.id] = user;
-            dialog.close();
-            form.reset();
-            return;
-        }
-        const { message, errors: json_err } = await result.json();
-        toasts.danger(message);
-        errors = json_err ?? {};
+		const [url, options] = user.id
+			? [
+					'api/users/' + user.id,
+					{
+						method: 'PUT',
+						body: JSON.stringify(user)
+					}
+			  ]
+			: [
+					'api/users',
+					{
+						method: 'POST',
+						body: JSON.stringify(user)
+					}
+			  ];
+		const result = await fetch_api(url, options);
+		if (result.status == 201 || result.status == 200) {
+			const user = await result.json();
+			users[user.id] = user;
+			dialog.close();
+			form.reset();
+			return;
+		}
+		const { message, errors: json_err } = await result.json();
+		toasts.danger(message);
+		errors = json_err ?? {};
 		if (result.status == 404) {
 			errors['name'] = errors['name'] ? [...errors['name'], message] : [message];
 			errors['username'] = errors['username'] ? [...errors['username'], message] : [message];
 			errors['email'] = errors['email'] ? [...errors['email'], message] : [message];
 		}
 	}
-    /**
-     * Delete user
-     * @param {number} id
-     */
+	/**
+	 * Delete user
+	 * @param {number} id
+	 */
 	async function handleDelete(id) {
-        errors = {};
-        const result = await fetch_api(`api/users/${id}`, {
-            method: 'DELETE'
-        });
-        if (result.status == 200) {
-            // @ts-ignore
-            users = Object.keys(users).reduce((acc,/** @type {number} */ key) => {
-                if (key != id) {
-                    // @ts-ignore
-                    acc[key] = users[key];
-                }
-                return acc;
-            }, {});
-            return;
-        }
-        const { message, errors: json_err } = await result.json();
-        toasts.danger(message);
+		errors = {};
+		const result = await fetch_api(`api/users/${id}`, {
+			method: 'DELETE'
+		});
+		if (result.status == 200) {
+			// @ts-ignore
+			users = Object.keys(users).reduce((acc, /** @type {number} */ key) => {
+				if (key != id) {
+					// @ts-ignore
+					acc[key] = users[key];
+				}
+				return acc;
+			}, {});
+			return;
+		}
+		const { message, errors: json_err } = await result.json();
+		toasts.danger(message);
 	}
-    function handleReset() {
-        user = {
-            id: null,
-            name: '',
-            username: '',
-            email: '',
-            password: '',
-            role_ids: []
-        };
-    }
+	function handleReset() {
+		user = {
+			id: null,
+			name: '',
+			username: '',
+			email: '',
+			password: '',
+			role_ids: []
+		};
+	}
 </script>
 
 <section class="container">
 	<article>
-
 		<header style="display: flex; align-items: center; justify-content: space-between;">
 			<h2>Users</h2>
-			<a href="#form" role="button" on:click={() => {
-				handleReset();
-				dialog.showModal();
-			}}>Add</a>
+			<a
+				href="#form"
+				role="button"
+				on:click={() => {
+					handleReset();
+					dialog.showModal();
+				}}>Add</a
+			>
 		</header>
 		<table>
 			<thead>
@@ -174,24 +186,42 @@
 				</tr>
 			</thead>
 			<tbody>
-				{#each Object.entries(users) as [_, { id, name, username, email, role_names }]}
+				{#if !loading}
+					{#each Object.entries(users) as [_, { id, name, username, email, role_names }]}
+						<tr>
+							<td>{name}</td>
+							<td>{username}</td>
+							<td>{email}</td>
+							<td>{role_names}</td>
+							<td class="flex">
+								<a
+									href="#edit"
+									class="contrast"
+									on:click={() => {
+										// @ts-ignore
+										user = { ...user, ...users[id] };
+										dialog.showModal();
+									}}
+									role="button">Edit</a
+								>
+								<a
+									href="#delete"
+									class="secondary"
+									on:click={() => {
+										handleDelete(id);
+									}}
+									role="button">Delete</a
+								>
+							</td>
+						</tr>
+					{/each}
+				{:else}
 					<tr>
-						<td>{name}</td>
-						<td>{username}</td>
-						<td>{email}</td>
-						<td>{role_names}</td>
-						<td class="flex">
-							<a href="#edit" class="contrast" on:click={() => {
-								// @ts-ignore
-								user = {...user, ...users[id]};
-								dialog.showModal();
-							}} role="button">Edit</a>
-							<a href="#delete" class="secondary" on:click={()=>{
-								handleDelete(id);
-							}} role="button">Delete</a>
+						<td colspan="5" style="">
+							<Loading />
 						</td>
 					</tr>
-				{/each}
+				{/if}
 			</tbody>
 		</table>
 	</article>
@@ -199,50 +229,61 @@
 <!-- Dialog form -->
 <dialog bind:this={dialog} style="width: 100%;" on:close={handleReset}>
 	<article style="width: 100%;">
-		<form method="dialog" bind:this={form} on:submit|preventDefault={handleSubmit} on:reset={handleReset} autocomplete="off">
-            <h2>{!user.id ? 'Create User' : 'Edit User'}</h2>
-            <pre>{JSON.stringify(user)}</pre>
+		<form
+			method="dialog"
+			bind:this={form}
+			on:submit|preventDefault={handleSubmit}
+			on:reset={handleReset}
+			autocomplete="off"
+		>
+			<h2>{!user.id ? 'Create User' : 'Edit User'}</h2>
 			<section class="grid">
 				<label for="name">Name</label>
-                <div>
-                    <input type="text" id="name" bind:value={user.name} required/>
-                    <FormError {errors} field="name" />
-                </div>
+				<div>
+					<input type="text" id="name" bind:value={user.name} required />
+					<FormError {errors} field="name" />
+				</div>
 			</section>
 			<section class="grid">
 				<label for="username">Username</label>
-                <div>
-                    <input type="text" id="username" bind:value={user.username} required/>
-                    <FormError {errors} field="username" />
-                </div>
+				<div>
+					<input type="text" id="username" bind:value={user.username} required />
+					<FormError {errors} field="username" />
+				</div>
 			</section>
 			<section class="grid">
 				<label for="email">E-mail</label>
-                <div>
-                    <input type="email" id="email" bind:value={user.email} required/>
-                    <FormError {errors} field="email" />
-                </div>
+				<div>
+					<input type="email" id="email" bind:value={user.email} required />
+					<FormError {errors} field="email" />
+				</div>
 			</section>
 			<section class="grid">
 				<label for="password">Password</label>
-                <div>
-                    <input type="password" id="password" bind:value={user.password} required={user.id == null} autocomplete="off"/>
-                    <FormError {errors} field="password" />
-                </div>
+				<div>
+					<input
+						type="password"
+						id="password"
+						bind:value={user.password}
+						required={user.id == null}
+						autocomplete="off"
+					/>
+					<FormError {errors} field="password" />
+				</div>
 			</section>
 			<section class="grid">
 				<label for="roles">Roles</label>
-                <div>
-                    {#each server_roles as { id, name }}
-                        <label>
-                            <input type="checkbox" id="roles" bind:group={user.role_ids} value={id} />
-                            {name}
-                        </label>
-                    {/each}
-                    <FormError {errors} field="role_ids" />
-                </div>
+				<div>
+					{#each server_roles as { id, name }}
+						<label>
+							<input type="checkbox" id="roles" bind:group={user.role_ids} value={id} />
+							{name}
+						</label>
+					{/each}
+					<FormError {errors} field="role_ids" />
+				</div>
 			</section>
-            <hr>
+			<hr />
 			<footer style="display: flex; flex-direction: row; justify-content: end; gap: 1rem">
 				<a
 					href="#cancel"
@@ -255,12 +296,7 @@
 				>
 					Close
 				</a>
-				<a
-					href="#submit"
-					class="primary"
-					role="button"
-					on:click={() => form.requestSubmit()}
-				>
+				<a href="#submit" class="primary" role="button" on:click={() => form.requestSubmit()}>
 					Submit
 				</a>
 			</footer>
