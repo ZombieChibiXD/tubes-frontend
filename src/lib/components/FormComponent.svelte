529290933f5f11a568a346836e8328488ac26684
Anonymous <anony@mou.us>
2023-06-26 21:42:25 +0800

    Added Form Component

---
+++ src/lib/components/FormComponent.svelte
@@ -0,0 +1,31 @@
+<script>
+	import FormError from "./FormError.svelte";
+
+    /**
+     * @type {string}
+     */
+    export let name;
+    
+    /**
+     * @type {string}
+     */
+    export let label = name;
+
+	/**
+	 * @type {{ [x: string]: string[]; }}
+	 */
+    export let errors;
+
+    /**
+     * @type {string}
+     */
+    export let field = name;
+</script>
+
+<section class="grid">
+    <label for={name}>{label}</label>
+    <div>
+        <slot field={field}/>
+        <FormError {errors} {field} />
+    </div>
+</section>
