9ad901ef531dd912fd44c2be4b0ba93ce97a16aa
Anonymous <anony@mou.us>
2023-06-30 01:04:29 +0800

    Changed about us.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,13 +11,20 @@
 		<header>
 			<h2>What is this Program About?</h2>
 			<p>
-				This is a simple program that allows you to manage your tools and their status.
+				This website is used to plan and log the usage of lathe machinary by estimating and overseeing the estimated life time during the process of lathing.
 			</p>
 		</header>
 		<main>
 			<h2>How to use it?</h2>
 			<p>
 				You first have to login to be able to use the program. If you don't have an account, you must request one to the administrator.
+			</p>
+			<p>
+				After logging in, you can start a new Project, or continue an existing one. You can also see the status of your tools and it's history on tool history.
+			</p>
+			<h3>As Administrator</h3>
+			<p>
+				As an admin, you can create new users, and manage the tools and projects.
 			</p>
 		</main>
 	</article>
