8f4ebf23e9b29e4b76a9f5e2bee29eedfe312b36
ZombieChibiXD <43260238+ZombieChibiXD@users.noreply.github.com>
2023-07-04 17:28:27 +0800

    Typable select

--- src/routes/project/ToolSelector.svelte
+++ src/routes/project/ToolSelector.svelte
@@ -1,5 +1,6 @@
 <script>
 	import FormError from '$lib/components/FormError.svelte';
+	import Select from 'svelte-select';
 
 	/**
 	 * @typedef { import("$lib/types/global").ToolProductToolbox & {
@@ -20,16 +21,6 @@
 	 */
 	export let materials = [];
 
-	// Detect if materials has changed
-	$: {
-		// Check if current material is in the list
-		if (!materials.find((item) => item.id == tool_material_id)) {
-			// If not, set current material to first item in the list
-			tool_material_id = '';
-		}
-		on_material_change();
-	}
-
 	/**
 	 * Type is key number and value is ToolColorCode
 	 * @type {{
@@ -39,17 +30,11 @@
 	export let colors = {};
 
 	/**
-	 * @type {ToolProductCurrent[]}
+	 * @type {(import("$lib/types/global").ToolMaterial & {
+	 * 		products?: ToolProductCurrent[]
+	 * })}
 	 */
-	let products = [];
-	/**
-	 * @type {ToolProductToolboxCurrent[]}
-	 */
-	let toolboxes = [];
-	/**
-	 * @type {import("$lib/types/global").ToolItem[]}
-	 */
-	let items = [];
+	let tool_material;
 
 	/**
 	 * @type {ToolProductCurrent | undefined}
@@ -71,48 +56,18 @@
 	 */
 	export let errors;
 
-	/**
-	 * @type {number | string}
-	 */
-	let tool_material_id;
-	/**
-	 * @type {number | string }
-	 */
-	let tool_product_id;
-	/**
-	 * @type {number | string }
-	 */
-	let tool_product_toolbox_id;
-	/**
-	 * @type {number | string }
-	 */
-	let tool_item_id;
-
-
-	const on_material_change = () => {
-		const material = materials.find((item) => item.id == tool_material_id);
-		products = material?.products ?? [];
-		tool_product_id = products.find((item) => item.id == tool_product_id) ? tool_product_id : '';
-		on_product_change();
+	const onChangeMaterial = (/** @type {any} */ _) => {
+		tool_product = undefined;
 	};
-
-	const on_product_change = () => {
-		tool_product = products.find((item) => item.id == tool_product_id);
-		toolboxes = tool_product?.toolboxes ?? [];
-		tool_product_toolbox_id = toolboxes.find((item) => item.id == tool_product_toolbox_id) ? tool_product_toolbox_id : '';
-		on_toolbox_change();
+	$: onChangeMaterial(tool_material);
+	const onChangeProduct = (/** @type {any} */ _) => {
+		tool_toolbox = undefined;
 	};
-
-	const on_toolbox_change = () => {
-		tool_toolbox = toolboxes.find((item) => item.id == tool_product_toolbox_id);
-		items = tool_toolbox?.tool_items ?? [];
-		tool_item_id = items.find((item) => item.id == tool_item_id) ? tool_item_id : '';
-		on_item_change();
+	$: onChangeProduct(tool_product);
+	const onChangeToolbox = (/** @type {any} */ _) => {
+		tool_item = undefined;
 	};
-
-	const on_item_change = () => {
-		tool_item = items.find((item) => item.id == tool_item_id);
-	};
+	$: onChangeToolbox(tool_toolbox);
 </script>
 
 <fieldset>
@@ -120,73 +75,67 @@
 	<section class="grid">
 		<label for="tool_material_id">Tool Material</label>
 		<div>
-			<select
+			<Select
+				itemId="id"
 				id="tool_material_id"
-				bind:value={tool_material_id}
-				on:change={on_material_change}
-				required
-			>
-				<option value="">
-					{#if !materials || materials.length == 0}
-						No Available Tool Materials Found
-					{:else}
-						Select Tool Material
-					{/if}
-				</option>
-				{#each materials as { id, name }}
-					<option value={id}>{name}</option>
-				{/each}
-			</select>
+				label="name"
+				containerStyles="height: 3rem;"
+				items={materials}
+				bind:value={tool_material}
+				listOffset={0}
+				placeholder={materials?.length == 0 ?? true
+					? 'No Available Tool Materials found'
+					: 'Select Tool Material'}
+				disabled={materials?.length == 0 ?? true}
+				required={true}
+				hasError={errors?.tool_material_id}
+			/>
 			<FormError {errors} field="tool_material_id" />
 		</div>
 	</section>
 	<section class="grid">
 		<label for="tool_product_id">Tool Code</label>
 		<div>
-			<select
+			<Select
+				itemId="id"
 				id="tool_product_id"
-				bind:value={tool_product_id}
-				on:change={on_product_change}
-				required
-			>
-				<option value="">
-					{#if !tool_material_id}
-						Select Tool Material First
-					{:else if products.length == 0}
-						No Available Tool Found
-					{:else}
-						Select Tool
-					{/if}
-				</option>
-				{#each products as { id, code, name }}
-					<option value={id}>{code}{name ? ' - ' + name : ''}</option>
-				{/each}
-			</select>
+				label="code"
+				containerStyles="height: 3rem;"
+				items={tool_material?.products}
+				bind:value={tool_product}
+				listOffset={0}
+				placeholder={tool_material
+					? tool_material?.products?.length
+						? 'Select Tool Material'
+						: 'No Available Tool found'
+					: 'Select Tool Material First'}
+				disabled={(tool_material?.products?.length ?? 0) == 0}
+				required={true}
+				hasError={errors?.tool_product_id}
+			/>
 			<FormError {errors} field="tool_product_id" />
 		</div>
 	</section>
 	<section class="grid">
 		<label for="tool_product_toolbox_id">Toolbox ID</label>
 		<div>
-			<select
+			<Select
+				itemId="id"
 				id="tool_product_toolbox_id"
-				bind:value={tool_product_toolbox_id}
-				on:change={on_toolbox_change}
-				required
-			>
-				<option value="">
-					{#if !tool_product_id}
-						Select Tool Code First
-					{:else if toolboxes.length == 0}
-						No Available Toolbox Found
-					{:else}
-						Select Toolbox
-					{/if}
-				</option>
-				{#each toolboxes as { id, code }}
-					<option value={id}>{tool_product?.prefix}-{code}</option>
-				{/each}
-			</select>
+				label="code"
+				containerStyles="height: 3rem;"
+				items={tool_product?.toolboxes}
+				bind:value={tool_toolbox}
+				listOffset={0}
+				placeholder={tool_product
+					? tool_product?.toolboxes?.length
+						? 'Select Toolbox'
+						: 'No Available Toolbox Found'
+					: 'Select Tool Code First'}
+				disabled={(tool_product?.toolboxes?.length ?? 0) == 0}
+				required={true}
+				hasError={errors?.tool_product_toolbox_id}
+			/>
 			<FormError {errors} field="tool_product_toolbox_id" />
 		</div>
 	</section>
@@ -195,38 +144,39 @@
 			Tool ID
 			{#if tool_item}
 				{@const { color } = colors[tool_item.tool_color_code_id]}
-				<span class="color-preview" style={`background-color: ${color};`}></span>
+				<span class="color-preview" style={`background-color: ${color};`} />
 			{/if}
 		</label>
 		<div>
-			<select id="tool_item_id" bind:value={tool_item_id} on:change={on_item_change} required>
-				<option value="" style="background-color: white; color: black;">
-					{#if !tool_product_toolbox_id}
-						Select Toolbox First
-					{:else if items.length == 0}
-						No Available ToolID/Color Found
-					{:else}
-						Select ToolID/Color
-					{/if}
-				</option>
-				{#each items as { id: item_id, tool_color_code_id: color_id }}
-					{@const { id, code, color, text_color } = colors[color_id]}
-					<option value={item_id} style="background-color: {color}; color: {text_color};"
-						>{code}{id}</option
-					>
-				{/each}
-			</select>
+			<Select
+				itemId="id"
+				id="tool_item_id"
+				label="id"
+				containerStyles="height: 3rem;"
+				items={tool_toolbox?.tool_items}
+				bind:value={tool_item}
+				listOffset={0}
+				placeholder={tool_toolbox
+					? tool_toolbox?.tool_items?.length
+						? 'Select ToolID/Color'
+						: 'No Available ToolID/Color Found'
+					: 'Select Toolbox First'}
+				disabled={(tool_toolbox?.tool_items?.length ?? 0) == 0}
+				required={true}
+				hasError={errors?.tool_item_id}
+			/>
 			<FormError {errors} field="tool_item_id" />
 		</div>
 	</section>
 </fieldset>
+
 <style>
-.color-preview {
-	display: inline-block;
-	width: 3em;
-	height: 1em;
-	border-radius: 1em;
-	border: 1px solid black;
-	margin-left: 1em;
-}
+	.color-preview {
+		display: inline-block;
+		width: 3em;
+		height: 1em;
+		border-radius: 1em;
+		border: 1px solid black;
+		margin-left: 1em;
+	}
 </style>
